43ddbb121e3c9db7d4908fe982e4d88f117d1a50
Phoenix Wu <phoenix@funpodium.net>
2018-04-02 20:49:43 +0800

    Initial Commit

---
+++ CS193P_Homework/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CS193P_Homework
+//
+//  Created by Phoenix Wu on H30/04/02.
+//  Copyright © 平成30年 Phoenix Wu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

fdbf750392ee928c8f03c373c150c4f58e297287
Phoenix Wu <phoenix@funpodium.net>
2018-04-04 09:53:29 +0800

    first version

---
+++ CS193P_Homework/Card.swift
@@ -0,0 +1,32 @@
+//
+//  Card.swift
+//  CS193P_Homework
+//
+//  Created by Phoenix Wu on H30/04/02.
+//  Copyright © 平成30年 Phoenix Wu. All rights reserved.
+//
+
+import Foundation
+
+struct Card {
+    
+    var isMatched = false
+    
+    var isFaceUp = false
+    
+    private static var identifierFactory = 0
+    
+    private static func getIdentifier() -> Int {
+        identifierFactory += 1
+        
+        return identifierFactory
+    }
+    
+    var identifier: Int
+    
+    init() {
+        identifier = Card.getIdentifier()
+    }
+    
+    
+}
---
+++ CS193P_Homework/Concentration.swift
@@ -0,0 +1,15 @@
+//
+//  Concentration.swift
+//  CS193P_Homework
+//
+//  Created by Phoenix Wu on H30/04/02.
+//  Copyright © 平成30年 Phoenix Wu. All rights reserved.
+//
+
+import Foundation
+
+class Concentration {
+    
+    var cards = [Card]()
+    
+}
